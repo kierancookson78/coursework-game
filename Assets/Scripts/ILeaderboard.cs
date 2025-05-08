9cfbc079bb5959cef5bdb4cf9f7793ade383f6d0
kierancookson78 <150475081+kierancookson78@users.noreply.github.com>
2025-05-08 16:23:40 +0100

    adding comments

--- Assets/Scripts/ILeaderboard.cs
+++ Assets/Scripts/ILeaderboard.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+// Implementation of the service locator pattern for a leaderboard adapted from "7_Singleton_ServiceLocator" by Sean Walton
+// at: https://canvas.swansea.ac.uk/courses/52796/files/7564778?module_item_id=3014035
 public interface ILeaderboard
 {
     void AddScore(int score);
--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -5,6 +5,9 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System;
 
+// Implementation of the service locator pattern for a leaderboard adapted from "7_Singleton_ServiceLocator" by Sean Walton
+// at: https://canvas.swansea.ac.uk/courses/52796/files/7564778?module_item_id=3014035
+
 [System.Serializable]
 public class LeaderboardData
 {
